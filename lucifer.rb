24b34b3c05f5a9b075b0fac67fdd121fa8ca0e4a
Teruonak <github@hotmail.com.br>
2013-03-08 03:39:42 -0300

    sketch of the game

--- lucifer.rb
+++ lucifer.rb
@@ -7,18 +7,25 @@
 
 class Lucifer
 	def initialize
-		perguntas = ['Esta e uma frase',
-			'Ola, sou outra frase']
 
+		# Frases para saudação
+		greetings = ['Faça logo sua pergunta',
+				'O que quer saber?']
 
-		greeting
+		# Frases padrões para serem escritas automaticamente quando for jogar
+		questions = ['Olá grande senhor do subsolo, eu gostaria de saber ',
+			'Mestre dos mestres, o senhor poderia me falar ']
+
+		letsPlay
 	end
 
-	def greeting
-		puts "Faça logo a sua pergunta!"
-	end
+	# Saudação do jogo.
+	def letsPlay
+		# ramdom para escrever uma das saudações do greetings
 
-	question = gets.chomp;
+		# iremos escrever a resposta, mas ao invés de aparecer o mesmo na tela, irá aparecer uma das perguntas.
+		question = gets.chomp;
+	end
 end
 
 lucifer = Lucifer.new
